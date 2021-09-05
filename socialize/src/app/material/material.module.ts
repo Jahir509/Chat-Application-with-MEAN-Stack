b300020d1eb2d1693b337cbfd1413c31dfba9213
jahir509 <me.jahirahmed@gmail.com>
2021-09-05 17:58:23 +0600

    Update Header and Material Module

--- socialize/src/app/material/material.module.ts
+++ socialize/src/app/material/material.module.ts
@@ -5,22 +5,64 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { FormsModule } from '@angular/forms';
 
+const MaterialComponents = [
+  MatButtonModule,
+  MatBadgeModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatListModule,
+  MatIconModule,
+  MatDividerModule,
+  MatExpansionModule,
+  LayoutModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatTooltipModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatListModule,
+  MatCardModule,
+  MatSlideToggleModule,
+  MatGridListModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatInputModule,
+  FormsModule
 
+]
 
 @NgModule({
   declarations: [
     HeaderComponent
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    MaterialComponents
   ],
   imports: [
     CommonModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatInputModule,
-  ]
+    MaterialComponents
+
+  ],
 })
 export class MaterialModule { }
